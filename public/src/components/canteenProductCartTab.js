6025ce96ccf38175034fa3d04a083b851edf9f78
KushanNalinka <127778421+KushanNalinka@users.noreply.github.com>
2024-08-13 09:54:09 +0530

    Update canteenProductCartTab.js

--- public/src/components/canteenProductCartTab.js
+++ public/src/components/canteenProductCartTab.js
@@ -1,48 +1,4 @@
-// import React from 'react' 
-// import { Link } from 'react-router-dom';
-// import { FaEdit } from "react-icons/fa";
-// import { useSelector, useDispatch } from 'react-redux';
-// import { addToCart } from '../stores/cart';
 
-// const ProductCart = (props) => {
-//     const carts = useSelector(store => store.cart.items);
-//     const {id, name, price, image,category,meal, slug, quantity, quantityChangedAt} = props.data;
-//     const mealText = Array.isArray(meal) ? meal.join(', ') : meal;
-
-//     const dispatch = useDispatch();
-//     const handleAddToCart = () => {
-//         dispatch(addToCart({
-//             productId: id,
-//             quantity: 1
-//         }));
-//         // Open the cart tab
-        
-//     }
-//     return (
-//     <div className='bg-white p-5 rounded-xl shadow-sm'>
-//         <Link to={slug}>
-//             <img src={image} alt='' className='w-full h-80 object-cover object-top drop-shadow-[0_80px_30px_#0007]' />
-//         </Link>
-//         <h2 className='text-2xl py-3 text-center font-medium'>{name}</h2>
-//         <p className='text-1xl py-1 text-center font-medium text-blue-500'>Last Quantity Update</p>
-//         <p className='text-1xl py-1 text-center font-medium'>{quantityChangedAt}</p>
-//         <p className='text-1xl py-1 text-center font-medium text-blue-500'>Available Quantity</p>
-//         <p className='text-1xl py-1 text-center font-medium'>{quantity}</p>
-        
-//         <div className='flex justify-between items-center'>
-            
-//             <button className='bg-[#FFC10C] p-2 rounded-md text-sm hover:bg-gray-400 flex gap-2' >
-//                 <img  FaEdit  alt="" className='w-5'/>
-//                 Add 
-//             </button>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default ProductCart
-
-// //onClick={}
 
 import React, { useState } from 'react'; 
 import { Link } from 'react-router-dom';
