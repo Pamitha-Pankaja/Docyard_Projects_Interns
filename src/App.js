546c11921edb9f7a769fe9e2fab51005a9ac1971
Lahiru Rajakaruna Jayasinghe <140371042+LahiruJ-2003@users.noreply.github.com>
2024-08-13 09:46:54 +0530

    Update App.js
    App.js Removed comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,4 @@
-//import './App.css';
-/*import Layout from './components/layout';
-import Home from './pages/home';
-import Detail from './pages/detail';
-import My from './pages/my';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/:slug' element={<Detail />} />
-        </Route>
-        <Route path='/my' element={<My/>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
-export default App;*/
 import Layout from './components/layout';
 import Home from './pages/home';
 import Detail from './pages/detail';
